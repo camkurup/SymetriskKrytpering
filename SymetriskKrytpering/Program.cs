c63042ab3a5caf3f1149d3cefadad4b1872b4680
camkurup <camkurup@hotmail.com>
2023-03-09 10:26:54 +0100

    -

--- SymetriskKrytpering/Program.cs
+++ SymetriskKrytpering/Program.cs
@@ -2,18 +2,22 @@
 using System.Text;
 
 //Setup encrytion and decryption
-var encryption = new Encryption();
-var key = encryption.GenerateRandomNumber(32);
-var iv = encryption.GenerateRandomNumber(16);
+var encryption = new AESEncryption();
+var randomNumberGenerator = new RandomNumberGenerator();
+
+//AES Encryption start
+var keyAES = randomNumberGenerator.GenerateRandomNumber(32);
+var ivAES = randomNumberGenerator.GenerateRandomNumber(16);
 
 var textToEncrpting = "Dette er den text jeg vil kryptere";
 
-var encrypted = encryption.Encrypt(Encoding.UTF8.GetBytes(textToEncrpting), key, iv);
-var decrypted = encryption.Decrypt(encrypted, key, iv);
+var encrypted = encryption.Encrypt(Encoding.UTF8.GetBytes(textToEncrpting), keyAES, ivAES);
+var decrypted = encryption.Decrypt(encrypted, keyAES, ivAES);
 
 var decryptedMessage = Encoding.UTF8.GetString(decrypted);
 
-//cosnole
+//cosnole 
 Console.WriteLine("Original Text: " + textToEncrpting);
 Console.WriteLine("Krypteret Text: " + Convert.ToBase64String(encrypted));
 Console.WriteLine("Dekrypteret Text: " + decryptedMessage);
+//AES Encryption end
---
+++ SymetriskKrytpering/RandomNumberGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymetriskKrytpering
+{
+    public class RandomNumberGenerator
+    {
+        public byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+    }
+}

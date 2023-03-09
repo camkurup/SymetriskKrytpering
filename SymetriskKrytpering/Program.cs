0336a009624e77a3ac8a1c7080003fe3ee814203
camkurup <camkurup@hotmail.com>
2023-03-09 10:20:41 +0100

    --

--- SymetriskKrytpering/Program.cs
+++ SymetriskKrytpering/Program.cs
@@ -1,8 +1,19 @@
-﻿
+﻿using SymetriskKrytpering;
+using System.Text;
 
-//start encrypting
+//Setup encrytion and decryption
+var encryption = new Encryption();
+var key = encryption.GenerateRandomNumber(32);
+var iv = encryption.GenerateRandomNumber(16);
+
 var textToEncrpting = "Dette er den text jeg vil kryptere";
 
+var encrypted = encryption.Encrypt(Encoding.UTF8.GetBytes(textToEncrpting), key, iv);
+var decrypted = encryption.Decrypt(encrypted, key, iv);
+
+var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+//cosnole
 Console.WriteLine("Original Text: " + textToEncrpting);
-Console.WriteLine("Krypteret Text: ");
-Console.WriteLine("Dekrypteret Text: ");
+Console.WriteLine("Krypteret Text: " + Convert.ToBase64String(encrypted));
+Console.WriteLine("Dekrypteret Text: " + decryptedMessage);

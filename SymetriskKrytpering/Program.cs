ee314cd9029e4826fe7c1654c046cefe68e2e2d4
camkurup <camkurup@hotmail.com>
2023-03-09 09:52:39 +0100

    -

--- SymetriskKrytpering/Program.cs
+++ SymetriskKrytpering/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//start encrypting
+var textToEncrpting = "Dette er den text jeg vil kryptere";
+
+Console.WriteLine("Original Text: " + textToEncrpting);
+Console.WriteLine("Krypteret Text: ");
+Console.WriteLine("Dekrypteret Text: ");
